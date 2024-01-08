5e657ce28afe9830b395ca08d18897afc94e3d48
IsaiahPage24 <ipage244@gmail.com>
2024-01-08 14:14:17 -0700

    Part two of prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your grade? (Percentage)\n");
+        string GradeInput = Console.ReadLine();
+        int Grade = int.Parse(GradeInput);
+        string LetterGrade = "K";
+        if (Grade >= 90)
+        {
+            LetterGrade = "A";
+        }
+        else if (Grade >= 80)
+        {
+            LetterGrade = "B";
+        }
+        else if (Grade >= 70)
+        {
+            LetterGrade = "C";
+        }
+        else if (Grade >= 60)
+        {
+            LetterGrade = "D";
+        }
+        else if (Grade < 60)
+        {
+            LetterGrade = "F";
+        }
+        Console.WriteLine($"Your letter grade is {LetterGrade}.");
     }
 }
