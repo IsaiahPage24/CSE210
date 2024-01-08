63a834452f0d1453240923e423ed6523e285bbd2
IsaiahPage24 <ipage244@gmail.com>
2024-01-08 12:30:52 -0700

    Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?\n");
+        string FirstName = Console.ReadLine();
+        Console.Write("What is your last name?\n");
+        string LastName = Console.ReadLine();
+        Console.WriteLine($"You are {LastName}, {FirstName} {LastName}.");
     }
 }
