7c3d13f6990049dfbf05b9f84094c7a77589d2e6
IsaiahPage24 <ipage244@gmail.com>
2024-01-17 11:42:31 -0700

    chore: Homework Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the Guessing Game!");
+        Console.Write("Choose a number: ");
+        string range = Console.ReadLine();
+        int i = int.Parse(range);
+        Console.WriteLine(string.Format("We will choose a random number between 1 and {0}", range));
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, i);
+        int guess = 0;
+        do
+        {
+            Console.Write("What is your guess?\n>");
+            string guessString = Console.ReadLine();
+            guess = int.Parse(guessString);
+            if (guess > number)
+            {
+                Console.WriteLine("Too High!");
+            }
+            if (guess < number)
+            {
+                Console.WriteLine("Too Low!");
+            }
+        } while (guess != number);
+        Console.Write("Correct!");
     }
 }
