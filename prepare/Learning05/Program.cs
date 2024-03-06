0c055facb600d24f2ca3f8fb3e14e17148fa860a
IsaiahPage24 <ipage244@gmail.com>
2024-03-06 12:31:24 -0700

    Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shape_list = new List<Shape>{};
+
+        Square square = new Square("blue", 5);
+        Circle circle = new Circle("red", 5);
+        Rectangle rectangle = new Rectangle("orange", 10, 15);
+
+        shape_list.Add(square);
+        shape_list.Add(circle);
+        shape_list.Add(rectangle);
+
+        foreach (Shape shape in shape_list)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
